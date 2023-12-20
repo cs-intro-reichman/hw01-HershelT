d43ab46ad34899eb3ddcc049857251c34245323e
= <hershelthomas@gmail.com>
2023-12-20 11:16:07 +0200

    Added comments to more complex code, as well as made doubles only display one decimal to pass git tests

--- GenThree.java
+++ GenThree.java
@@ -7,18 +7,18 @@ import java.util.Random;
 public class GenThree {
     public static void main(String[] args) {
         // Put your code here
-        int min = Integer.parseInt(args[0]);
-        int max = Integer.parseInt(args[1]);
-        gen3(min, max);
+        int minNumber = Integer.parseInt(args[0]);
+        int maxNumber = Integer.parseInt(args[1]);
+        gen3(minNumber, maxNumber);
     }
     public static void gen3(int min, int max) {
 
         Random random = new Random();
-        int mini = random.ints(min, max)
-                .limit(3)
-                .peek(System.out::println)
+        int mini = random.ints(min, max) //Using Stream declares random range
+                .limit(3) //sets the limit of how many random numbers
+                .peek(System.out::println)//uses .peek to print out each random number on new line
                 .min()
-                .getAsInt();
+                .getAsInt(); //uses .min function to get the minimum number and sets it to int mini
         System.out.println("The minimal number generated was " + mini);
     }
 }
